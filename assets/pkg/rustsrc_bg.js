5895878d5916b06a0bdb17a5779af177d9420931
samonjourus <bsta7599@vt.edu>
2021-11-28 16:16:29 -0500

    i hate gitignore...

---
+++ assets/pkg/rustsrc_bg.js
@@ -0,0 +1,20 @@
+import * as wasm from './rustsrc_bg.wasm';
+
+/**
+*/
+export function bubbleSortTest() {
+    wasm.bubbleSortTest();
+}
+
+/**
+*/
+export function arrayAdditionTest() {
+    wasm.arrayAdditionTest();
+}
+
+/**
+*/
+export function hashTest() {
+    wasm.hashTest();
+}
+
---
+++ assets/pkg/rustsrc_bg.wasm.d.ts
@@ -0,0 +1,6 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function bubbleSortTest(): void;
+export function arrayAdditionTest(): void;
+export function hashTest(): void;
